7e1436dcf3911b67548f51125c164c1c27b6cd75
Arfan Uddin <arfan.rfn@gmail.com>
2019-02-17 02:51:48 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	var typed = new Typed('.typed', {
+        stringsElement: '.typed-js',
+        typeSpeed: 20,
+        backSpeed: 30,
+        loop: true,
+        // shuffle: true,
+        loopCount: Infinity,
+        showCursor: true,
+      });
+});
